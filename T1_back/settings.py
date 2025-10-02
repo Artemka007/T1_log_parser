0fcaa781aebd3a2a2d0e2463034c65393a1dd697
Artem Kopylov <artem.kopylov@aimestech.com>
2025-10-02 14:30:40 +0500

    init

--- T1_back/settings.py
+++
@@ -1,3 +0,0 @@
-UPLOAD_FOLDER = 'uploads'  # Directory to save uploaded files
-ALLOWED_EXTENSIONS = {'json'}
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Limit file size (e.g., 16MB)
--- T1_front/README.md
+++
@@ -1,4 +0,0 @@
-### Run app
-```bash
-docker run -p 3000:3000 tf-front
-```
